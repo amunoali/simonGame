c76652afcfc498f911964bd101049615ba643a35
amunoali <amunoali@gmail.com>
2022-04-21 21:06:59 -0400

    js addition

--- js/game.js
+++ js/game.js
@@ -2,21 +2,72 @@ let buttoncolors = ["red", "blue", "green", "yellow"]
 let gamePattern = []
 let userClickedPattern = []
 
-function nextSequence(){
-    let randomNumber = Math.floor(Math.random()* 4) 
-   
-    let randomChosenColor = buttoncolors[randomNumber]
-    gamePattern.push(randomChosenColor)
+let started = false
+let level = 0
 
-    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
-    var audio = new Audio("./audio/" + randomChosenColor + ".mp3")
-    audio.play()
+$(document).keydown(function() {
+    if (!started){
+        $("#level-title").text("Level " + level)
+        nextSequence()
+        started = true
+    }
+})
+$(".btn").click(function(){
+    let usedChosencolor = $(this).attr("id")
+    userClickedPattern.push(userchosenColor)
+    playSound(userchosenColor)
+    checkAnswer(userClickedPattern.length - 1)
+})
 
-    $("button").on("click", function(){
+function checkAnswer(currentLevel){
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel])
+    console.log("success")
+    if (userClickedPattern.length === gamePattern.length){
+    setTimeout(function(){
+        nextSequence()
+    }, 100)
+} 
+} {
+    console.log("wrong")
+    playSound("wrong")
+    $("body").addClass("game-over")
+    setTimeout(function(){
+    }, 200)
 
- audio.play()
-        userClickedPattern.push(randomChosenColor)
-    })
+    $("Level-title").text("Game Over, Press Any Key to Restart")
+    
 }
+function nextSequence() {
 
-
+    userClickedPattern = [];
+    level++;
+    $("#level-title").text("Level " + level);
+  
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+  
+    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+  }
+  
+  function playSound(name) {
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+  }
+  
+  function animatePress(currentColor) {
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function () {
+      $("#" + currentColor).removeClass("pressed");
+    }, 100);
+  }
+  
+  //1. Create a new function called startOver().
+  function startOver() {
+  
+    //3. Inside this function, you'll need to reset the values of level, gamePattern and started variables.
+    level = 0;
+    gamePattern = [];
+    started = false;
+  }
